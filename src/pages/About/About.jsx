75cd3b5977853a5e96f5b6b942cf9072918ec59e
brs14shn <brsshn292929@gmail.com>
2022-07-13 20:32:50 +0300

    first commit

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AboutStyle from "./About.style";
+
+const About = () => {
+  return <div>About</div>;
+};
+
+export default About;
---
+++ src/pages/Login/Lojin.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginStyle from "./Lojin.style";
+
+const Lojin = () => {
+  return <div>Lojin</div>;
+};
+
+export default Lojin;
