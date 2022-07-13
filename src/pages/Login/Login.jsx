4a431348de6fc64ab18724d940b2e3e30c1ae968
brs14shn <brsshn292929@gmail.com>
2022-07-13 22:05:36 +0300

    Loign added && session.localstorage

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,29 @@
-import React from "react";
-import LoginStyle from "./Login.style";
+import LoginStyle, {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+  StyledButton,
+} from "./Login.style";
+
+import mealSvg from "../../assets/meal.svg";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={mealSvg} />
+        <Header>{"<CW/>"}Recipe</Header>
+        <StyledForm>
+          <StyledInput type="text" placeholder="username" required />
+          <StyledInput type="password" placeholder="password" required />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
 };
 
 export default Login;
