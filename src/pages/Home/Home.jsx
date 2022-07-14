e329f249859bddc117b193e32acdfdc0400b3e5b
brs14shn <brsshn292929@gmail.com>
2022-07-14 21:15:05 +0300

    Home selectedMealType added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import HomeStyle from "./Home.style";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Header from "../../components/Header/Header";
 
 const Home = () => {
+  //! USESTATE
   const selectedMealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
   const [recipes, setRecipes] = useState();
   const [query, setquery] = useState("egg");
   const [mealType, setmealType] = useState(selectedMealType[0]);
 
+  //! APİ QUERY
   let API_KEY = "280a9f0207045a7b1eb040071263aca6";
   let API_ID = "742d542a";
   const url = `https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}&mealType=${mealType}`;
 
+  //! ASYNC-AWAİT
   const getData = async () => {
     try {
       const { data } = await axios.get(url);
@@ -23,7 +26,8 @@ const Home = () => {
     }
   };
 
-  console.log(recipes);
+  // console.log(recipes);
+  //* Header gönderilen recipesları Card componetine basmadan kontrol etmek için kullanılır
 
   // useEffect(() => {
   //   getData();
