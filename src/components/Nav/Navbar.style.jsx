16458f199c531d484edf950ed0ae24749f25bcf9
brs14shn <brsshn292929@gmail.com>
2022-07-14 20:06:10 +0300

    bug fixed

--- src/components/Nav/Navbar.style.jsx
+++ src/components/Nav/Navbar.style.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Flex from "../../components/globalStyles/Flex";
+import Flex from "../../components/GlobalStyles/Flex";
 import { Link } from "react-router-dom";
 
 const Nav = styled(Flex)`
--- src/pages/About/About.style.jsx
+++ src/pages/About/About.style.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Flex from "../../components/globalStyles/Flex";
+import Flex from "../../components/GlobalStyles/Flex";
 
 export const AboutContainer = styled(Flex)`
   flex-direction: column;
