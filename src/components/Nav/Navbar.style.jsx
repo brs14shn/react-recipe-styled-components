65518343f9330917d68583e2ba7cd317efcee6fe
brs14shn <brsshn292929@gmail.com>
2022-07-14 20:02:13 +0300

    styled updated

--- src/components/Nav/Navbar.style.jsx
+++ src/components/Nav/Navbar.style.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-import Flex from "../../components/GlobalStyles/Flex";
+import Flex from "../../components/globalStyles/Flex";
 import { Link } from "react-router-dom";
 
 const Nav = styled(Flex)`
   padding: 1rem 2rem;
-  background-color: ${({ theme }) => theme.colors.navbarBgColor};
+  background: ${({ theme }) => theme.colors.navbarBgColor};
 `;
 
 export const Logo = styled(Link)`
@@ -19,12 +19,22 @@ export const Logo = styled(Link)`
   }
 `;
 
+export const Hamburger = styled.div`
+  display: none;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.mainColor};
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    display: block;
+  }
+`;
+
 export const Menu = styled(Flex)`
   @media (max-width: ${({ theme }) => theme.screens.lg}) {
     flex-direction: column;
     width: 100%;
     max-height: ${({ isOpen }) => (isOpen ? "400px" : "0")};
     overflow: hidden;
+    transition: max-height 0.3s ease-in;
   }
 `;
 
@@ -39,15 +49,9 @@ export const MenuLink = styled(Link)`
     color: ${({ theme }) => theme.colors.mainColor};
     font-weight: bold;
   }
-`;
-
-export const Hamburger = styled.div`
-  display: none;
-  cursor: pointer;
-  color: ${({ theme }) => theme.colors.mainColor};
   @media (max-width: ${({ theme }) => theme.screens.lg}) {
-    display: block;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.mainColor};
+    width: 100%;
   }
 `;
-
 export default Nav;
